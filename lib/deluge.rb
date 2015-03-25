0a9a5f0b6688c1da73f382ce918246786c2a56f0
James Sherwood-Jones <james.sherwoodjones@gmail.com>
2015-03-25 19:56:16 +0000

    Added gitignore. Improved comments.

--- lib/deluge.rb
+++ lib/deluge.rb
@@ -2,11 +2,24 @@ require 'httparty'
 require 'json'
 require 'deluge/response'
 
-# Uses the Deluge web ui's JSON calls to interact with deluged
+##
+# Uses the Deluge web UI's JSON calls to interact with deluged.
 class Deluge
-  attr_accessor :last_request, :session_id, :host
+  ##
+  # The last request that was made to the web UI.
+  attr_accessor :last_request
 
-  # The data we want back when getting all torrent info
+  ##
+  # The current session ID (if any), used to authenticate requests.
+  attr_accessor :session_id
+
+  ##
+  # The host that we're connecting to. This is a URL in the form:
+  #  http[s]://my-deluge-web-ui:port/json
+  attr_accessor :host
+
+  ##
+  # The data we want back when getting all torrent info.
   UPDATE_UI_PARAMS = %w(queue name total_size state
                         progress num_seeds total_seeds
                         num_peers total_peers
@@ -18,13 +31,18 @@ class Deluge
                         max_download_speed max_upload_speed
                         seeds_peers_ratio)
 
-  # Creates a new deluge instance which will connect to the given host
+  ##
+  # Creates a new Deluge instance to connect to the given host.
   def initialize(host)
     @host = host
     @last_request = {}
     @last_id = 0
   end
 
+  ##
+  # Authenticates with the web UI by sending the given password & then connects to the first available host.
+  #
+  # The +password+ is set on the web UI under "Preferences" -> "Interface".
   def login(password)
     send_request('auth.login', [password])
 
@@ -44,25 +62,27 @@ class Deluge
     response.result && !session_id.nil?
   end
 
+  ##
+  # Checks for session ID (set when authentication succeeds).
   def logged_in?
     !@session_id.nil?
   end
 
-  # Retrieves the list of current torrents
+  # Retrieves the list of current torrents.
   def torrents
     response = send_request('web.update_ui', [UPDATE_UI_PARAMS, {}])
 
     response.result['torrents']
   end
 
-  # Starts downloading a torrent URL
+  # Starts downloading a torrent URL (either a path to a torrent file or a magnet link).
   def add_torrent_url(url, download_location, move_completed_path)
     params = add_torrent_params(url, download_location, move_completed_path)
 
     send_request('web.add_torrents', params).result
   end
 
-  # Sends a JSON request to Deluge
+  # Sends a raw JSON request to Deluge. Used by other methods to make calls (e.g. adding a new torrent).
   def send_request(method, params = [])
     @last_id += 1
 
@@ -79,12 +99,12 @@ class Deluge
 
   private
 
-  # Creates the header dictionary used for requests
+  # Creates the header dictionary used for requests (contains the session ID used for authentication).
   def headers(session_id)
     { 'Cookie' => "_session_id=#{session_id}" }
   end
 
-  # Assembles the parameters used when adding a torrent URL
+  # Assembles the parameters used when adding a torrent URL.
   def add_torrent_params(url, download_location, move_completed_path)
     ['0' => {
       options: {
